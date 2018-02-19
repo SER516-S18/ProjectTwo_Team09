a6de964e4a196f66d35122bd72e015b8e10968da
Adhiraj Tikku <33083990+adhiraj92@users.noreply.github.com>
2018-02-18 23:57:31 -0700

    Created new GraphTesting.java file
    Initial commit

---
+++ src/app/client/GraphTesting.java
@@ -0,0 +1,34 @@
+package app.client;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class GraphTesting {
+	public void Test() {
+		JFrame testFrame = new JFrame("Graph Test"); 
+		testFrame.setTitle("Line chart");
+		testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		testFrame.add(new Graph(ClientCommonData.getInstance()));
+		testFrame.pack();
+		testFrame.setVisible(true);
+	}
+	
+	public void createTestData() {
+		ArrayList<ArrayList<Integer>> listOLists = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> singleList = new ArrayList<Integer>();
+		singleList.add(1);
+		singleList.add(2);
+		singleList.add(3);
+		singleList.add(4);
+		singleList.add(5);
+		listOLists.add(singleList);
+		ClientCommonData.getInstance().setDataFromServer(listOLists);
+	}
+	
+	public static void main(String[] args) {
+		GraphTesting graphTest = new GraphTesting();
+		graphTest.createTestData();
+		graphTest.Test();
+	}
+}
