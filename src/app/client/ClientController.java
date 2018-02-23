d5652d0066af7d09293e645528ecd4386d73a8aa
“Vishakha-singal” <vishakha-singal@github.com>
2018-02-22 20:10:13 -0700

    Changes for client socket

--- src/app/client/ClientController.java
+++ src/app/client/ClientController.java
@@ -15,37 +15,36 @@ public class ClientController {
 	Socket clientSocket;
 	private int portNo;
 	private String hostName;
-	private int channelCount;
 	private PrintWriter outputStream;
 	private BufferedReader inputReader;
 
 	public ClientController(int portNo) {
 		this.portNo = portNo;
-		this.channelCount = 0;
 		this.hostName = "localhost";
 		this.clientSocket = null;
 		outputStream = null;
 		inputReader = null;
+		this.dataSetFromServer = new ArrayList<String>();
 	}
 
+	
+	public void clearDataInArrayList() {
+		synchronized(this) {
+			this.dataSetFromServer.clear();
+		}
+	}
 
-	// x1,y1;x2,y2;...number of channels
-	private void readFromServer() {
+	private boolean readFromServer() {
 		String inputLine = null;
 		try {
 			while ((inputLine = this.inputReader.readLine()) != null) {
-				
-				//TODO: Now have to convert it to DataModel. will do after confirmed data style from server
-				
-				System.out.println(inputLine);
-				if (inputLine.indexOf(";") >= 0 && this.channelCount > 0) {
-					String[] parts = inputLine.split(";");
-					
-				}
+				this.dataSetFromServer.add(inputLine);
 			}
+			return true;
 		} catch (IOException e) {
 			// Print on console
 			System.err.println("IO Exception: " + hostName);
+			return false;
 		}
 		
 	}
@@ -55,7 +54,11 @@ public class ClientController {
 			this.clientSocket = new Socket(hostName, portNo);
 			this.outputStream = new PrintWriter(this.clientSocket.getOutputStream(), true);
 			this.inputReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-			this.readFromServer();
+			this.outputStream.println(channelNo);
+			boolean didReadSuccess = true;
+			while(didReadSuccess) {
+				didReadSuccess = this.readFromServer();
+			}
 		} catch (UnknownHostException e) {
 			// Print on console
 			System.err.println("Unknown host: " + hostName);
