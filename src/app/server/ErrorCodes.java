d7aedd4ad4f5a98895142736f96ba3a72b18632a
sangeetha151294 <sangeetha151294@gmail.com>
2018-02-23 16:29:40 -0700

    Update ErrorCodes.java

--- src/app/server/ErrorCodes.java
+++ src/app/server/ErrorCodes.java
@@ -8,17 +8,12 @@ public class ErrorCodes {
 	private static HashMap<String, String> errorDescriptionHash = new HashMap<String, String>();
 	// Error with error codes
 
-	public static final String N101 = "N101"; 
-
 	static {
-		errorDescriptionHash.put(N101, "Please enter a valid number");
+		errorDescriptionHash.put("N101", "Please enter a valid number");
+		errorDescriptionHash.put("N102", "The highest value must be greater than or equal to the lowest value.");
 	}
 
-	public static final String N102 = "N102"; 
-
-	static {
-		errorDescriptionHash.put(N102, "The highest value must be greater than or equal to the lowest value.");
-	}
+	
 
 	
 	public static String printErrorMessage(String errorCode) {
