d5642b5eb10569e02b6bf3a9f760dd1c7cf285da
sangeetha151294 <sangeetha151294@gmail.com>
2018-02-23 15:58:29 -0700

    Update ServerNetworkThread.java

--- src/app/server/ServerNetworkThread.java
+++ src/app/server/ServerNetworkThread.java
@@ -59,7 +59,7 @@ public class ServerNetworkThread extends Thread {
         }
         catch (IOException e) {
             // TODO: Log this error in the console.
-        	if(e.getMessage().equals("socket closed")) {
+        	if(e.getMessage().equalsIgnoreCase("socket closed")) {
         		new ServerException("Info: "+e.getMessage());
         	}else {
         		new ServerException(e.toString());
