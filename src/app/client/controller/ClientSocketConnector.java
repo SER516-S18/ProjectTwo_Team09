833ab484792394643890930f1723e56e272326bd
sangeetha151294 <sangeetha151294@gmail.com>
2018-02-23 15:33:29 -0700

    Update ClientSocketConnector.java

--- src/app/client/controller/ClientSocketConnector.java
+++ src/app/client/controller/ClientSocketConnector.java
@@ -15,7 +15,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import app.client.gui.CoordinatesModel;
 import app.client.model.ClientCommonData;
 import app.client.model.LogConstants;
-import app.server.ServerSocketImpl;
+
 
 public class ClientSocketConnector implements Runnable {
 
