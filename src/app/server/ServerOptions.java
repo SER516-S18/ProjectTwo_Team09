0a9806bd44491ffd2d34a442e7a95f72de0c3272
jananit24 <35354715+jananit24@users.noreply.github.com>
2018-02-23 22:08:41 -0700

    Update ServerOptions.java

--- src/app/server/ServerOptions.java
+++ src/app/server/ServerOptions.java
@@ -1,11 +1,27 @@
 package app.server;
 
+/**
+ * Common class to set the data values like 
+ * minimum value, maximum value and frequency
+ * 
+ * @author Nelson Tran
+ * @version 1.0
+ * @since 2018-02-23
+ * 
+ */
 public class ServerOptions {
 
 	public final int minimumValue;
 	public final int maximumValue;
 	public final int frequency;
 
+	/**
+	 * Sets the server options - max value, min value and frequency *
+	 * @param minimumValue - maximum value set at the server
+	 * @param maximumValue - minimum value set at the server
+	 * @param frequency - frequency at which data is sent
+	 *
+	 */
 	public ServerOptions(int minimumValue, int maximumValue, int frequency) {
 		this.minimumValue = minimumValue;
 		this.maximumValue = maximumValue;
