24932089bc44cee419a5dbecc8f1e44ddbb28f98
jananit24 <35354715+jananit24@users.noreply.github.com>
2018-02-23 19:35:37 -0700

    Refactor Server.java

--- src/app/server/Server.java
+++ src/app/server/Server.java
@@ -3,17 +3,15 @@ package app.server;
 import app.server.ServerController;
 import app.server.ServerView;
 
+/**
+ * Define the network connection port according to the port
+ * number defined in the PowerPoint slides.
+ * 
+ * @author Nelson Tran
+ * @version 1.0
+ */
 public class Server {
-
-    /**
-     * Define the network connection port according to the port
-     * number defined in the PowerPoint slides.
-     * 
-     * @author Nelson Tran
-     * @version 1.0
-     */
     private static int PORT = 1516;
-
     /**
      * Main entry point for the server.
      */
@@ -22,5 +20,4 @@ public class Server {
         ServerController controller = new ServerController(PORT, view);
         view.setController(controller);
     }
-
 }
